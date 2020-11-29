934269d65d56ac59aac3d50c015a22e6e0445171
Mayank Nagora <50271010+itsmemayank@users.noreply.github.com>
2020-11-29 19:16:05 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>
+      My Favourite Movies Genre
+      <ul>
+        <li>Psychological</li>
+        <li>Thriller</li>
+        <li>Mystery</li>
+        <li>Suspence</li>
+      </ul>
+    </h1>
+  </div>,
+  document.getElementById("root")
+);
