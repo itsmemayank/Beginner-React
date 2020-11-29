c3e49abfa38c095fe05bc0f4acc801f23f79b2ef
Mayank Nagora <50271010+itsmemayank@users.noreply.github.com>
2020-11-29 19:46:59 +0530

    javascript using jsx

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+const fname = "Mayank";
+const lname = "Nagora";
+
 ReactDom.render(
   <div>
-    <h1>My Favourite Movies Genre</h1>
-    <ul>
-      <li>Psychological</li>
-      <li>Thriller</li>
-      <li>Mystery</li>
-      <li>Suspence</li>
-    </ul>
+    <h1>Hello {fname + " " + lname}!</h1>
+    <p>My lucky no is {Math.ceil(Math.random() * 100)}</p>
   </div>,
   document.getElementById("root")
 );
